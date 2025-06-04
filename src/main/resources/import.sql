21bf189c657eac035e1db97882db3acfb84f9ec1
Luiz Barros <lck.barros@gmail.com>
2025-06-04 20:41:03 -0300

    fix: inserting category_id fk seed into tb_activity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,8 +6,8 @@ INSERT INTO tb_attendant (name, email) VALUES ('Tiago Faria', 'tiago@gmail.com')
 INSERT INTO tb_attendant (name, email) VALUES ('Maria do Rosário', 'maria@gmail.com');
 INSERT INTO tb_attendant (name, email) VALUES ('Teresa Silva', 'teresa@gmail.com');
 
-INSERT INTO tb_activity (name, description, price) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00);
-INSERT INTO tb_activity (name, description, price) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.00);
+INSERT INTO tb_activity (name, description, price, category_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00, 1);
+INSERT INTO tb_activity (name, description, price, category_id) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.00, 2);
 
 INSERT INTO tb_block (start_time, end_time, activity_id) VALUES ('2017-09-25 08:00:00', '2017-09-25 11:00:00', 1);
 INSERT INTO tb_block (start_time, end_time, activity_id) VALUES ('2017-09-25 14:00:00', '2017-09-25 18:00:00', 2);
